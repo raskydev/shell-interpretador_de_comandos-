6bf087cff8e153de5b82539d0b8f935698496d7d
Rayane k. <0117243@academico.ifmg.edu.br>
2026-05-08 22:42:19 +0000

    parser com suporte a pipes usando strtok

--- rush.c
+++ rush.c
@@ -1,7 +1,7 @@
 // para usar funções do linux
 #define _POSIX_C_SOURCE 200809L
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,32 +10,45 @@ int main(void){
 
     while (1){
 
-    //prompt
+        // prompt
         printf("$");
         fgets(input, 1024, stdin);
-    //remove o \n do final     
+        // remove o \n do final
         input[strcspn(input, "\n")] = '\0';
 
-        if (strcmp(input, "exit") == 0){
+        if (strcmp(input, "exit") == 0)
+        {
 
             break;
         }
-    //quebra o programa em tokens
-    char *token = strtok(input, " ");
-    char *argv[64];
-    int argc = 0;
+        char *argv[64];
+        int argc = 0;
+        char *saveptr1;
+        char *segmento = strtok_r(input, "|", &saveptr1);
+        
+        //separa segmentos por "|"
+        while (segmento != NULL){
+            
+            printf("segmento: '%s'\n", segmento);
+            char *saveptr2;
+            char *token = strtok_r(segmento, " ", &saveptr2);
+            
+            // quebra o programa em tokens por espaço
+            while (token != NULL){
     
-        while (token != NULL){
+                argv[argc] = token;
+                printf("token: %s\n", token);
+                argc++;
+                token = strtok_r(NULL, " ", &saveptr2);
 
-            argv[argc] = token; 
-            token = strtok(NULL, " ");
-            argc++;
+            }
 
+            segmento = strtok_r(NULL, "|", &saveptr1);
         }
+       
         argv[argc] = NULL;
     }
 
-    
-    return 0;
+
+return 0;
 }
- 
